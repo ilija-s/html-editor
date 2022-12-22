3284a8ef217ea9f4a0bc50a2b489436f069a82f9
Ilija Stojanovic <mi19077@alas.matf.bg.ac.rs>
2022-12-22 13:13:46 +0100

    Improve: Fixed formatting to follow LLVM standard

--- src/search/editorsearch.h
+++ src/search/editorsearch.h
@@ -1,18 +1,18 @@
 #ifndef EDITORSEARCH_H
 #define EDITORSEARCH_H
 
-#include <QWidget>
 #include <QTextDocument>
+#include <QWidget>
 
-class EditorSearch : public QWidget
-{
-    Q_OBJECT
+class EditorSearch : public QWidget {
+  Q_OBJECT
 
 public:
-    EditorSearch(QWidget *parent = nullptr);
+  EditorSearch(QWidget *parent = nullptr);
 
 public slots:
-    void onSearchButtonClicked(const QString& searchString, QTextDocument *document);
+  void onSearchButtonClicked(const QString &searchString,
+                             QTextDocument *document);
 
 private:
 };
