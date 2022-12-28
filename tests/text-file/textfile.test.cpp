f872311939d7dfe6f3a9b2f969bf0757681af431
Ilija Stojanovic <mi19077@alas.matf.bg.ac.rs>
2022-12-28 11:08:09 +0100

    New: Added tests for TextFile class

---
+++ tests/text-file/textfile.test.cpp
@@ -0,0 +1,38 @@
+#include "../src/text-file/textfile.h"
+#include "tests/libs/catch2/catch.hpp"
+#include <QFile>
+
+TEST_CASE("Test the constructor of TextFile", "[TextFile]") {
+  TextFile file("test.txt", "/path/to/test.txt");
+  REQUIRE(file.filename() == "test.txt");
+  REQUIRE(file.absoluteFilePath() == "/path/to/test.txt");
+}
+
+TEST_CASE("Test the content function of TextFile") {
+  TextFile file("test.txt", "./test.txt");
+
+  // Set up a mock file with known content
+  QFile mockFile("./test.txt");
+  mockFile.open(QIODevice::WriteOnly);
+  mockFile.write("Hello, world!\nThis is a test file.\n");
+  mockFile.close();
+
+  REQUIRE(file.content() == "Hello, world!\nThis is a test file.\n");
+}
+
+TEST_CASE("Test the find function of TextFile") {
+  TextFile file("test.txt", "./test.txt");
+
+  // Set up a mock file with known content
+  QFile mockFile("./test.txt");
+  mockFile.open(QIODevice::WriteOnly);
+  mockFile.write("Hello, world!\nThis is a test file.\n");
+  mockFile.close();
+
+  QVector<LineData> matches = file.find("test");
+  REQUIRE(matches.size() == 1);
+  REQUIRE(matches[0].filename == "test.txt");
+  REQUIRE(matches[0].absoluteFilePath == "./test.txt");
+  REQUIRE(matches[0].lineNumber == 2);
+  REQUIRE(matches[0].content == "This is a test file.");
+}
