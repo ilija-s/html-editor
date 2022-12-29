f94908574ac3452565c7540b2619107e6af36790
Natalija <natafil20@gmail.com>
2022-12-29 17:10:29 +0100

    New: new test cases for search

--- tests/searchTest/tst_search.cpp
+++ tests/searchTest/tst_search.cpp
@@ -7,23 +7,78 @@
 
 TEST_CASE("My first test with Catch2", "[fancy]")
 {
-    int argc = 0;
-    QApplication a{argc, nullptr};
-    QWidget *parent = nullptr;
-    EditorSearch editorSearch1(parent);
+    SECTION("Search for word that appears just once"){
+        int argc = 0;
+        QApplication a{argc, nullptr};
+        QWidget *parent = nullptr;
+        EditorSearch editorSearch1(parent);
 
+        QTextDocument document;
+        document.setPlainText("This is a test document with some test words.");
+        QString word = "document";
 
-    QTextDocument document;
-    document.setPlainText("This is a test document with some test words.");
-    QString searchString = "test";
+        editorSearch1.onSearchButtonClicked(word, &document);
 
-    editorSearch1.onSearchButtonClicked(searchString, &document);
+        QTextCursor cursor(&document);
+        cursor.movePosition(QTextCursor::Start);
+        cursor = document.find(word, cursor, QTextDocument::FindWholeWords);
+        REQUIRE(cursor.charFormat().background().color() == Qt::yellow);
+        REQUIRE(cursor.charFormat().foreground().color() == Qt::black);
 
-    QTextCursor cursor(&document);
-    cursor.movePosition(QTextCursor::Start);
-    int count = 0;
-    while (!cursor.isNull() && !cursor.atEnd()) {
-        cursor = document.find(searchString, cursor, QTextDocument::FindWholeWords);
+    }
+
+    SECTION("Found words are highlighted"){
+        int argc = 0;
+        QApplication a{argc, nullptr};
+        QWidget *parent = nullptr;
+        EditorSearch editorSearch1(parent);
+
+        QTextDocument document;
+        document.setPlainText("This is a test document with some test words.");
+        QString word = "test";
+
+        editorSearch1.onSearchButtonClicked(word, &document);
+
+        QTextCursor cursor(&document);
+        cursor.movePosition(QTextCursor::Start);
+        int count = 0;
+        while (!cursor.isNull() && !cursor.atEnd()) {
+            cursor = document.find(word, cursor, QTextDocument::FindWholeWords);
+            if (!cursor.isNull()) {
+              count++;
+              QTextCharFormat format = cursor.charFormat();
+              REQUIRE(format.background().color() == Qt::yellow);
+              REQUIRE(format.foreground().color() == Qt::black);
+            }
+          }
+
+        //The word appears 2 times
+        REQUIRE(count == 2);
+    }
+
+    SECTION("Highlights are reset - first word for search is not highlighted anymore and the second one is highlighted"){
+
+      int argc = 0;
+      QApplication a{argc, nullptr};
+      QWidget *parent = nullptr;
+      EditorSearch editorSearch1(parent);
+
+      QTextDocument document;
+      document.setPlainText("This is a test document with some test words.");
+      QString word = "test";
+
+      //First search
+      editorSearch1.onSearchButtonClicked(word, &document);
+
+      //Second search
+      word = "words";
+      editorSearch1.onSearchButtonClicked(word, &document);
+
+      QTextCursor cursor(&document);
+      cursor.movePosition(QTextCursor::Start);
+      int count = 0;
+      while (!cursor.isNull() && !cursor.atEnd()) {
+        cursor = document.find(word, cursor, QTextDocument::FindWholeWords);
         if (!cursor.isNull()) {
           count++;
           QTextCharFormat format = cursor.charFormat();
@@ -32,6 +87,26 @@ TEST_CASE("My first test with Catch2", "[fancy]")
         }
       }
 
-    //The word appears 2 times
-    REQUIRE(count == 2);
+      //We have 2 'test' and 1 'words', so if count is 2 or 3 then the old search is not reset
+      REQUIRE(count == 1);
+    }
+
+    SECTION("Search for word that does not exists"){
+        int argc = 0;
+        QApplication a{argc, nullptr};
+        QWidget *parent = nullptr;
+        EditorSearch editorSearch1(parent);
+
+        QTextDocument document;
+        document.setPlainText("This is a test document with some test words.");
+        QString word = "potato";
+
+        editorSearch1.onSearchButtonClicked(word, &document);
+
+        QTextCursor cursor(&document);
+        cursor.movePosition(QTextCursor::Start);
+        cursor = document.find(word, cursor, QTextDocument::FindWholeWords);
+        REQUIRE(cursor.charFormat().background().color() == Qt::transparent);
+        REQUIRE(cursor.isNull() == true);
+    }
 }
