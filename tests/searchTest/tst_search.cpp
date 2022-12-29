2d7e5eda9f6f771336aeaaaed1eddae774284632
Natalija <natafil20@gmail.com>
2022-12-29 14:29:50 +0100

    New: new test case for search

--- tests/searchTest/tst_search.cpp
+++ tests/searchTest/tst_search.cpp
@@ -2,6 +2,8 @@
 #include "../../src/search/editorsearch.h"
 
 #include <QApplication>
+#include <QTextCursor>
+#include <QTextCharFormat>
 
 TEST_CASE("My first test with Catch2", "[fancy]")
 {
@@ -9,5 +11,27 @@ TEST_CASE("My first test with Catch2", "[fancy]")
     QApplication a{argc, nullptr};
     QWidget *parent = nullptr;
     EditorSearch editorSearch1(parent);
-    REQUIRE(0 == 0);
+
+
+    QTextDocument document;
+    document.setPlainText("This is a test document with some test words.");
+    QString searchString = "test";
+
+    editorSearch1.onSearchButtonClicked(searchString, &document);
+
+    QTextCursor cursor(&document);
+    cursor.movePosition(QTextCursor::Start);
+    int count = 0;
+    while (!cursor.isNull() && !cursor.atEnd()) {
+        cursor = document.find(searchString, cursor, QTextDocument::FindWholeWords);
+        if (!cursor.isNull()) {
+          count++;
+          QTextCharFormat format = cursor.charFormat();
+          REQUIRE(format.background().color() == Qt::yellow);
+          REQUIRE(format.foreground().color() == Qt::black);
+        }
+      }
+
+    //The word appears 2 times
+    REQUIRE(count == 2);
 }
