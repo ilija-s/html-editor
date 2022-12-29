4b0dc54c956c65811b13492a68a7ac02e9c5e580
Natalija <natafil20@gmail.com>
2022-12-29 14:22:14 +0100

    Fix: test case

--- tests/searchTest/tst_search.cpp
+++ tests/searchTest/tst_search.cpp
@@ -1,8 +1,13 @@
 #include <catch2/catch.hpp>
 #include "../../src/search/editorsearch.h"
 
+#include <QApplication>
+
 TEST_CASE("My first test with Catch2", "[fancy]")
 {
-    EditorSearch editorSearch1;
+    int argc = 0;
+    QApplication a{argc, nullptr};
+    QWidget *parent = nullptr;
+    EditorSearch editorSearch1(parent);
     REQUIRE(0 == 0);
 }
