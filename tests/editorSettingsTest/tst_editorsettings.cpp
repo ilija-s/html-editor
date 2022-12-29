74a7c387e8bbd30cb6bd0e761caba385d345e4e6
Natalija <natafil20@gmail.com>
2022-12-29 22:10:03 +0100

    Fix: test case fixed

--- tests/editorSettingsTest/tst_editorsettings.cpp
+++ tests/editorSettingsTest/tst_editorsettings.cpp
@@ -2,14 +2,14 @@
 #include "../../src/editorsettings/editorsettings.h"
 
 #include <QApplication>
-#include <QWidget>
+#include <QDialog>
 
 TEST_CASE("My first test with Catch2", "[fancy]")
 {
     SECTION("Test fFontSize function") {
         int argc = 0;
         QApplication a{argc, nullptr};
-        QWidget *parent = nullptr;
+        QDialog *parent = nullptr;
         EditorSettings editorSettings(parent);
 
         editorSettings.fFontSize();
